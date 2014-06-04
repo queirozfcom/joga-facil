353b943b65e3e7bd33bbbc0dc6734be0b91d126d
Felipe <falmeida1988@gmail.com>
2014-06-04 11:44:31 -0300

    Testes iniciais.

--- spec/models/pelada_spec.rb
+++ spec/models/pelada_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Pelada do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be instantiated" do
+
+    p = Pelada.new
+
+    p.data = DateTime.new
+    p.local = 'copacabana'
+    p.minimo_pessoas = 10
+    p.responsavel = 'foo@bar.com'
+
+    p.save!
+
+    expect(Pelada.where(local:'copacabana',minimo_pessoas: 10).take).not_to eq nil
+
+
+
+  end
 end
